743f98e17dcf6544f2e4cdf27ae232534d2b6b30
betaagussetiyana <betamobile78@gmail.com>
2024-01-29 23:20:29 +0700

    tata:update js memberikan comment dan keterangan dari js

--- program/login.js
+++ program/login.js
@@ -1,20 +1,19 @@
 // login.js
 
-document
-  .getElementById("login-form")
-  .addEventListener("submit", function (event) {
-    event.preventDefault();
+// Menambahkan event listener pada form login
+document.getElementById("login-form").addEventListener("submit", function (event) {
+  event.preventDefault(); // Mencegah pengiriman form dan reload halaman
 
-    // Validasi kredensial pengguna (contoh sederhana)
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
+  // Mendapatkan nilai dari input username dan password
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
 
-    // Implementasi validasi login sesuai kebutuhan
+  // Implementasi validasi login sesuai kebutuhan
 
-    // Contoh sederhana: jika login berhasil, arahkan ke halaman peminjaman buku
-    if (username === "admin" && password === "admin") {
-      window.location.href = "index.html";
-    } else {
-      alert("Login gagal. Silakan coba lagi.");
-    }
-  });
+  // Contoh sederhana: jika login berhasil, arahkan ke halaman peminjaman buku
+  if (username === "admin" && password === "admin") {
+    window.location.href = "index.html"; // Mengarahkan ke halaman peminjaman buku
+  } else {
+    alert("Login gagal. Silakan coba lagi."); // Menampilkan alert jika login gagal
+  }
+});
